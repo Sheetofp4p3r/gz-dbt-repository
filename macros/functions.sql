06ff2942917768e1c09b4613fc4327ee82d2d13a
Samuel Carlier <samuel.carlier@gmail.com>
2025-05-14 13:39:54 +0000

    macro

---
+++ macros/functions.sql
@@ -0,0 +1,9 @@
+{% macro margin_percent(revenue, purchase_cost, precision=2) %}
+ROUND(
+        SAFE_DIVIDE(
+            ({{ revenue }} - {{ purchase_cost }}),
+            {{ revenue }}
+        ) * 100,
+        {{ precision }}
+    )
+{% endmacro %}
--- models/staging/intermediate/int_sales_margin.sql
+++ models/staging/intermediate/int_sales_margin.sql
@@ -1,12 +1,26 @@
-  SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
-      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-  FROM {{ref("stg_raw__sales")}} s
-  LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+WITH base_calculations AS (
+    SELECT
+        products_id,
+        date_date,
+        orders_id,
+        revenue,
+        quantity,
+        purchase_price,
+        quantity * purchase_price AS raw_purchase_cost,
+        revenue - (quantity * purchase_price) AS raw_margin
+    FROM {{ ref("stg_raw__sales") }} s
+    LEFT JOIN {{ ref("stg_raw__product") }} p
+        USING (products_id)
+)
+
+SELECT
+    products_id,
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    purchase_price,
+    ROUND(raw_purchase_cost, 2) AS purchase_cost,
+    ROUND(raw_margin, 2) AS margin,
+    {{ margin_percent('revenue', 'raw_purchase_cost') }} AS margin_percent
+FROM base_calculations
